21ab5ae1bd835ca240d93a6144c2e9d9be93bdfe
Jeff Codling <circuitjc@gmail.com>
2015-09-01 15:00:57 -0400

    HistoryData_schema.js: Added timestamp documentation to history data

--- schemas/HistoryData_schema.js
+++ schemas/HistoryData_schema.js
@@ -1,8 +1,8 @@
 var HistoryData_Schema = {
     name: "HistoryData",
     fields: [
-        { name: "dataValues", isArray: true, fieldType:"DataValue" },
-        { name: "timestamp", isArray: true, fieldType:"DateTime"}
+        { name: "dataValues",       isArray: true, fieldType:"DataValue", documentation: "An array of values of history data for the Node. The size of the array depends on the requested data parameters."},
+        { name: "timestamp",        isArray: true, fieldType:"UtcTime"}
     ]
 };
 exports.HistoryData_Schema = HistoryData_Schema;
