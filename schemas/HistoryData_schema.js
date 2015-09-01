ca68525f4bd1820662f5accbc5054b6d75755554
Jeff Codling <circuitjc@gmail.com>
2015-09-01 14:58:00 -0400

    HistoryData_schema.js: Added timestamp to history data

--- schemas/HistoryData_schema.js
+++ schemas/HistoryData_schema.js
@@ -1,7 +1,8 @@
 var HistoryData_Schema = {
     name: "HistoryData",
     fields: [
-        { name: "dataValues", isArray: true, fieldType:"DataValue" }
+        { name: "dataValues", isArray: true, fieldType:"DataValue" },
+        { name: "timestamp", isArray: true, fieldType:"DateTime"}
     ]
 };
 exports.HistoryData_Schema = HistoryData_Schema;
